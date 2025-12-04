81a877faf5a357450940f6027629ccc17bb2d11b
Bhargavi Naik <168259289+bvnaik05@users.noreply.github.com>
2025-12-04 11:22:28 +0530

    Update Firebase config with placeholder values
    Replaced Firebase configuration with placeholders.

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // PASTE YOUR FIREBASE CONFIG HERE
 const firebaseConfig = {
-  apiKey: "AIzaSyCCj46O2uFy8iU24GmAzm1K_KNkobNbql8",
-  authDomain: "portfoliomate-mini.firebaseapp.com",
-  projectId: "portfoliomate-mini",
-  storageBucket: "portfoliomate-mini.firebasestorage.app",
-  messagingSenderId: "585532340586",
-  appId: "1:585532340586:web:668130af54d70820024d13"
+  apiKey: "YOUR API KEY",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
